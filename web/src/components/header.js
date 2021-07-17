0cd305b7a018837dcb556112f7c611abeaf51913
noor <noortshepo@gmail.com>
2021-07-17 21:34:54 +0200

    adited header

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -4,14 +4,17 @@ import React from "react";
 import * as styles from "./header.module.css";
 
 const Header = () => (
-  <div className={styles.branding}>
-    <Link to="/">
-      {/* <img
-        className={styles.imageLogo}
-        src="https://pbs.twimg.com/profile_images/1413509493187563526/TQFn-RjL_400x400.jpg"
-        alt="logo"
-      /> */}
-    </Link>
+  <div className={styles.center}>
+    <div className={styles.logoContainer}>
+      <Link to="/">
+        <img
+          className={styles.imageLogo}
+          src="https://pbs.twimg.com/profile_images/1413509493187563526/TQFn-RjL_400x400.jpg"
+          alt="logo"
+        />
+      </Link>
+    </div>
+    <div className={styles.strip}></div>
   </div>
 );
 
