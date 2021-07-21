4430989a70ec163c1308a3622b037d703ae62e05
noor <noortshepo@gmail.com>
2021-07-21 02:22:26 +0200

    using different seo component

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -7,7 +7,18 @@ const clientConfig = require("./client-config");
 
 const isProd = process.env.NODE_ENV === "production";
 
+// const siteMetadata = {
+//   title: "Footy ZA",
+//   titleTemplate: "%s · Football's home",
+//   description: "Football blog.",
+//   url: "https://footyza.netlify.app", // No trailing slash allowed!
+//   image:
+//     "https://cdn.sanity.io/images/3rw4tk2q/production/c277cc856aab9f6f5a878ac31c74ad8deb8d5e76-1350x759.jpg?rect=0,0,1349,759&w=1200&h=675&fit=crop&auto=format", // Path to your image you placed in the 'static' folder
+//   twitterUsername: "@mosia_fpl",
+// };
+
 module.exports = {
+  // siteMetadata: { ...siteMetadata },
   plugins: [
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
---
+++ web/src/components/seo2.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { useLocation } from "@reach/router";
+import { useStaticQuery, graphql } from "gatsby";
+
+const SEO = () => {
+    const { pathname } = useLocation()
+
+    const seo = {
+        title: "Footy ZA",
+        description: "Football blog by Mosia Noor-Tshepo",
+        image: `siteMetadata`,
+        url: `https://footyza.netlify.app/${pathname}`,
+    }
+
+    return (
+        <Helmet title={seo.title} titleTemplate={seo.title}>
+            <meta name="description" content={seo.description} />
+            <meta name="image" content={seo.image} />
+
+            {seo.url && <meta property="og:url" content={seo.url} />}
+
+            {<meta property="og:type" content="article" />}
+
+            {seo.title && <meta property="og:title" content={seo.title} />}
+
+            {seo.description && (
+                <meta property="og:description" content={seo.description} />
+            )}
+
+            {seo.image && <meta property="og:image" content={seo.image} />}
+
+            <meta name="twitter:card" content="summary_large_image" />
+
+            {"@mosia_fpl" && (
+                <meta name="twitter:creator" content="@mosia_fpl" />
+            )}
+
+            {seo.title && <meta name="twitter:title" content={seo.title} />}
+
+            {seo.description && (
+                <meta name="twitter:description" content={seo.description} />
+            )}
+
+            {seo.image && <meta name="twitter:image" content={seo.image} />}
+        </Helmet>
+    )
+}
+
+export default SEO
+
+SEO.propTypes = {
+    title: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+    article: PropTypes.bool,
+}
+
+SEO.defaultProps = {
+    title: null,
+    description: null,
+    image: null,
+    article: false,
+}
+
+// const query = graphql`
+//   query SEO {
+//     site {
+//       siteMetadata {
+//         defaultTitle: title
+//         titleTemplate
+//         defaultDescription: description
+//         siteUrl: url
+//         defaultImage: image
+//         twitterUsername
+//       }
+//     }
+//   }
+// `;
