c3f87b3bdddfc4f0ad75e56a215d94c4eaaf8c5f
noor <noortshepo@gmail.com>
2021-07-25 01:05:58 +0200

    removed home SEO image

--- web/src/components/blog-post-preview-list.js
+++ web/src/components/blog-post-preview-list.js
@@ -6,11 +6,6 @@ import SEO from "../components/seo";
 function BlogPostPreviewGrid(props) {
   return (
     <div className={styles.root}>
-      <SEO
-        title="footy ZA"
-        description="Football blog by Mosia Noor-Tshepo"
-        image="https://cdn.sanity.io/images/3rw4tk2q/production/c277cc856aab9f6f5a878ac31c74ad8deb8d5e76-1350x759.jpg?rect=0,0,1349,759&w=1200&h=675&fit=crop&auto=format"
-      />
       {/* <div className={styles.Banner}>
         <img
           src="https://res.cloudinary.com/ddqp6ml5t/image/upload/v1626561512/banner_g7m2ny.png"
